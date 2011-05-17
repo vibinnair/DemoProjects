2182f439c743038f295575a53ecdfc832bb48270
Vibin Nair <vibin.nair@gmail.com>
2011-05-17 19:38:28 +0530

    Done with demo app

---
+++ vendor/bundle_gems/ruby/1.9.1/bin/rake2thor.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"ruby.exe" "D:/WorkArea/Research/Ruby/RUBY_O~1/PRACTI~1/demo_app/vendor/bundle_gems/ruby/1.9.1/bin/rake2thor" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"ruby.exe" "%~dpn0" %*
